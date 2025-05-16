ae3d328dd1b4331899b4835cca741bfb026a87ae
Bhavya <bhavyaky0690@gmail.com>
2025-05-16 12:24:03 +0530

    Added DjangoDEBUG environ var.

--- src/inpro/settings.py
+++ src/inpro/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&o+cf)3th&-i!v1z14yw!36)2u)0f^bmptqyq1x1&d7org6&p-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get("DEBUG")).lower()=="true"
+print("DEBUG", DEBUG, type(DEBUG))
 
 ALLOWED_HOSTS = [
     ".railway.app" # https://saas.prod.railway.app
